82a0e0cc3b093e56de659f6b5de0c96e8a30b120
newcoder310 <61921140+newcoder310@users.noreply.github.com>
2023-11-26 04:04:49 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,52 +1,73 @@
 #!/bin/bash
 
 config_file="config.txt"
+GREEN='\033[0;32m'  # ANSI color code for green
+RED='\033[0;31m'    # ANSI color code for red
+NC='\033[0m'        # ANSI color code to reset color
 
 # Read the previous user selection from the config file
 if [ -e "$config_file" ]; then
-    selected_options=$(cat "$config_file")
+    existing_options=$(cat "$config_file")
 else
-    selected_options=""
+    existing_options=""
 fi
 
 # Define dynamic options with corresponding names and URLs
 options=(
-    ["1"]="csm=https://csm.example.com"
-    ["2"]="Infra=https://infra.example.com"
-    ["3"]="producer=https://producer.example.com"
+    "csm=https://csm.example.com"
+    "Infra=https://infra.example.com"
+    "producer=https://producer.example.com"
 )
 
-# Filter out options that are already present in config.txt
-for selected_option in $(echo "$selected_options" | tr ',' ' '); do
-    unset "options[$selected_option]"
-done
-
-# Display only the names to the user
-echo "Select an option:"
-for i in "${!options[@]}"; do
-    IFS='=' read -ra option_parts <<< "${options[$i]}"
-    name=${option_parts[0]}
-    echo "$i. $name"
-done
+# Display the names to the user with option numbers, including a message for existing options
+function display_menu {
+    echo "Select an option:"
+    index=1
+    for option in "${options[@]}"; do
+        name=${option%=*}
+        if grep -q "$name" <<< "$existing_options"; then
+            echo -e "${index}. ${name} ${GREEN}-- You already have this${NC}"
+        else
+            echo "${index}. $name"
+        fi
+        index=$((index + 1))
+    done
+}
 
 # Get user input
-read -p "Enter your choice(s) separated by commas: " user_choices
+while true; do
+    display_menu
+    read -p "Enter your choice(s) separated by commas: " user_choices
 
-# Save the user's selection to the config file
-echo "$selected_options,$user_choices" > "$config_file"
+    # Save the user's selection to the config file
+    IFS=',' read -ra choices_array <<< "$user_choices"
+    invalid_choice=false
+
+    for choice in "${choices_array[@]}"; do
+        selected_option=${options[$((choice - 1))]}
+        selected_name=${selected_option%=*}
+
+        if grep -q "$selected_name" <<< "$existing_options"; then
+            echo -e "${RED}You selected an option for a feature that already exists. Please try again!${NC}"
+            invalid_choice=true
+            break
+        fi
+
+        existing_options+="$selected_name"$'\n'
+    done
+
+    if [ "$invalid_choice" = false ]; then
+        break
+    fi
+done
+
+# Write the selected options back to the config file
+echo -e "$existing_options" > "$config_file"
 
 # Process the user's choices dynamically
-IFS=',' read -ra choices_array <<< "$user_choices"
-
 for choice in "${choices_array[@]}"; do
-    selected_option=${options[$choice]}
-    if [ -n "$selected_option" ]; then
-        IFS='=' read -ra option_parts <<< "$selected_option"
-        name=${option_parts[0]}
-        url=${option_parts[1]}
-        # Call update.sh with arguments for each selected option
-        ./update.sh "$name" "$url"
-    else
-        echo "Invalid choice: $choice"
-    fi
+    selected_option=${options[$((choice - 1))]}
+    url=${selected_option#*=}
+    # Call update.sh with arguments for each selected option
+    ./update.sh "$choice" "$url"
 done
